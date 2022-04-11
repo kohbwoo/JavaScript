09aeb10cf0eaae14a2fad4cad940f581ea8f8514
ByungWoo Koh <kohbwoo@gmail.com>
2022-04-12 02:32:58 +0900

    commit

--- .TEST/DeepCopy.js
+++ .TEST/DeepCopy.js
@@ -1,8 +1,6 @@
 function deepCopy(persons) { 
     result = [ {name : persons[0].name, age : persons[0].age}, {name : persons[1].name, age : persons[1].age} ] 
     return result
-
-
 } 
 let persons1 =[ { name: "홍길동", age: 16 }, { name: "임꺽정", age: 18 } ]; 
 let persons2 = deepCopy(persons1); 
